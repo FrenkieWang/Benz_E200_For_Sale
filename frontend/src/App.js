42cfffe0faff1922b8b26c2b8a987f835c52197f
FrenkieWang <FrenkieWang21@gmail.com>
2024-01-18 21:14:26 +0000

    Initialize the project

---
+++ frontend/src/App.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import YouTube from 'react-youtube';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import compare1 from './images/compare1.jpg'; 
+import compare2 from './images/compare2.jpg'; 
+import NCT_Tax from './images/NCT_Tax.jpg'; 
+import WeChat from './images/WeChat.jpg';
+import WhatsApp from './images/WhatsApp.jpg';
+
+import ImageSlider from './ImageSlider';
+
+function App() {
+  const [language, setLanguage] = useState('zh'); // 默认为英文
+
+  function toggleLanguage() {
+    setLanguage(language === 'zh' ? 'en' : 'zh');
+  }
+
+  function _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <div className="App" style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      alignItems: 'center', 
+      justifyContent: 'flex-start',
+      height: '100vh', // 这将使div占满整个视口的高度
+      overflow: 'auto'
+    }}>
+      <h1 style = {{color : 'brown'}}>
+        {language === 'en' ? 'Mercedes Benz E200 Kompressor for Sale' : '出二手 奔驰 E200 Kompressor'}
+      </h1>
+      <button 
+        onClick={toggleLanguage}
+        style={{
+          backgroundColor: 'brown', // 灰色背景
+          color: 'white', // 棕色文字
+          borderColor: 'black', // 棕色边框
+          borderWidth: '1px',
+          borderStyle: 'solid'
+        }}
+      >
+        {language === 'en' ? '切换到中文' : 'Switch to English'}
+      </button>
+      <br />
+      <p style = {{color : 'brown' , borderStyle: 'solid', borderWidth: '3px', borderColor: 'brown', padding: '5px'}}>
+        {language === 'en' ? (
+          <>
+            Fujiwara's <b>Nightmare</b> <br /> Suzuki's <b>Favourite</b>
+          </>
+        ) : (
+          <>
+            藤原拓海的<b>噩梦</b> <br /> 茂木夏树的<b>最爱</b>
+          </>
+        )}
+      </p>
+      <h2 style = {{color : 'brown'}}>
+        {language === 'en' ? 'Car Image ' : '汽车图片'}
+      </h2>
+      <div>
+        <img src={compare1} alt="Description" width="300" height="400" />
+        <img src={compare2} alt="Description" width="300" height="400" />
+      </div>
+      <br />
+      <ImageSlider />
+      <h3 style = {{color : 'brown'}}>
+        {language === 'en' ? 'NCT and Road Tax -- Still Valid!' : '车检+保险 -- 还没有过期！'}
+      </h3>
+      <img src={NCT_Tax} alt="Description" width="300" height="400"/>
+      <h3 style = {{color : 'brown'}}>
+        {language === 'en' ? 'Classic Animation Review -- Initial D' : '头文字D 经典动画回顾'}
+      </h3>
+      <YouTube videoId="-OTUgn_RVzo" opts={opts} onReady={_onReady} />
+      <h3 style = {{color : 'brown'}}>
+        {language === 'en' ? 'Initial Price : Only 2000 Euro ' : '初始价: 只要2000欧'}
+      </h3>
+      <h4> {language === 'en' ? 'Please Contact: Gavin ' : '请联系Gavin'}</h4>
+      {language === 'en' ? 
+         <img src={WhatsApp} alt="Description" width="300" height="400"/> : 
+         <img src={WeChat} alt="Description" width="300" height="400"/>}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/ImageSlider.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import car1 from './images/car1.jpg'; 
+import car2 from './images/car2.jpg'; 
+import car3 from './images/car3.jpg'; 
+import car4 from './images/car4.jpg'; 
+import car5 from './images/car5.jpg'; 
+
+function ImageSlider() {
+    const images = [ 
+        car1, car2, car3, car4, car5
+      ];
+    
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrevious = () => {
+    const isFirstImage = currentIndex === 0;
+    const newIndex = isFirstImage ? images.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastImage = currentIndex === images.length - 1;
+    const newIndex = isLastImage ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  return (
+    <div>
+    <button className="btn" 
+        onClick={goToPrevious}
+    >⬅</button>
+    <img src={images[currentIndex]} alt={`Slide ${currentIndex}`} width="500" height="400" style={{ border: '5px solid brown' }}/>
+    <button className="btn" 
+        onClick={goToPrevious}
+    >➡</button>
+    </div>
+  );
+}
+
+export default ImageSlider;
